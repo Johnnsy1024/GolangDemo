f22a3b4a19466f6427ea8181fef3a89ed729b2a7
Johnnsy <fzx401@gmail.com>
2025-06-11 23:10:50 +0800

    update golang relevant things

--- sortAlgorithm/bubbleSort.go
+++ sortAlgorithm/bubbleSort.go
@@ -1,11 +1,11 @@
 package sortAlgorithm
 
-func BubbleSort(ll []int) {
-	n := len(ll)
+func BubbleSort(arr []int) {
+	n := len(arr)
 	for i := 0; i < n-1; i++ {
 		for j := 0; j < n-i-1; j++ {
-			if ll[j] > ll[j+1] {
-				ll[j], ll[j+1] = ll[j+1], ll[j]
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
 			}
 		}
 	}
